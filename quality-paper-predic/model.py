f3303dd0c309697f2fc6e36ff21d347a12954d84
Sigit Permadi <116578793+permadi1053@users.noreply.github.com>
2023-12-23 02:59:43 +0700

    Create model.py

---
+++ quality-paper-predic/model.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import joblib
+
+def train_and_save_model():
+    # Membaca data dari CSV
+    df = pd.read_csv("generated_data_sorted_integer2.csv")
+
+    # Memisahkan fitur (X) dan label (y)
+    X = df.drop('quality', axis=1)
+    y = df['quality']
+
+    # Membagi data menjadi set pelatihan dan set pengujian
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+    
+
+    # Inisialisasi model KNeighborsClassifier
+    model = KNeighborsClassifier(n_neighbors=3)
+
+    # Melatih model
+    model.fit(X_train, y_train)
+
+    # Menyimpan model ke file
+    joblib.dump(model, 'KNeighborsClassifier1.pkl')
+
+if __name__ == "__main__":
+    train_and_save_model()
